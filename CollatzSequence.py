02226a06d374a1c93731727360148b5f537b1123
TheDeluxo <55024213+TheDeluxo@users.noreply.github.com>
2021-04-22 16:43:21 +0300

    StackOverflow ftw
    ¯\_(⊙︿⊙)_/¯

--- CollatzSequence.py
+++ CollatzSequence.py
@@ -5,51 +5,49 @@
 import sys
 
 def collatz(num):
-    try:
-        num = int(num)
-        print ("Last number in the sequence is " + str(num))
-        if num == 1:
-            print("The number is already 1! Exiting...")
+    num = int(num)
+    print ("Last number in the sequence is " + str(num))
+    if num == 1:
+        print("The number is already 1! Exiting...")
+        sys.exit()
+    elif num % 2 == 0:
+        print("This number is even")
+        res = num // 2
+        print("The result after the last iteration is " + str(res) + "\n")
+        if res == 1:
+            print("We've reached 1! Exiting...")
             sys.exit()
-        elif num % 2 == 0:
-            print("This number is even")
-            res = num // 2
-            print("The result after the last iteration is " + str(res) + "\n")
+        while res != 1:
+            res = collatz(res)
             if res == 1:
                 print("We've reached 1! Exiting...")
                 sys.exit()
-            while res != 1:
-                collatz(res)
-                res = collatz(res)
-                if res == 1:
-                    print("We've reached 1! Exiting...")
-                    # break
-                    sys.exit()
-        elif num % 2 == 1:
-            print("This number is odd")
-            res = 3 * num + 1
-            print("The result after the last iteration is " + str(res) + "\n")
+    elif num % 2 == 1:
+        print("This number is odd")
+        res = 3 * num + 1
+        print("The result after the last iteration is " + str(res) + "\n")
+        if res == 1:
+            print("We've reached 1! Exiting...")
+            sys.exit()
+        while res != 1:
+            collatz(res)
+            res = collatz(res)
             if res == 1:
                 print("We've reached 1! Exiting...")
                 sys.exit()
-            while res != 1:
-                collatz(res)
-                res = collatz(res)
-                if res == 1:
-                    print("We've reached 1! Exiting...")
-                    # break
-                    sys.exit()
-    except ValueError:
-        print ("I said A NUMBER!!!")
     return res
 
 
 print("Welcome to the Collatz Sequencer!")
-print("Please type a whole number to begin!")
+# print("Please type a whole number to begin!")
+while True:
+    try:
+        for n in collatz(int(input("Please type a whole number to begin with!\n"))):
+            print ("You've entered " + n + "for a starting number")
+        break
+    except ValueError:
+        print("I said A NUMBER!!!")
 
-inp = int(input())
-
-collatz(inp)
 
 # The task is described at the end of the page at this url:
 # https://automatetheboringstuff.com/2e/chapter3/
